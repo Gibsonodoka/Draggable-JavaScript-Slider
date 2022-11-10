831bf752db151bb1c2d1afa0384a09ce493b1946
Gibson uwhetu <vellagibbs@gmail.com>
2022-11-10 16:24:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 const carousel = document.querySelector(".carousel");
 
+let isDragStart = false;
+
+const dragStart = () => {
+    isDragStart = true;
+}
+
 const dragging = (e) => {
+    if(!isDragStart) return;
+    e.previousDefault();
     carousel.scrollLeft = e.pageX;
 }
 
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
