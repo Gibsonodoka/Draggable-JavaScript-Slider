08b5caee147b757b7c82fe91e927c6ae639dd616
Gibson uwhetu <vellagibbs@gmail.com>
2022-11-11 12:36:38 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,15 +3,17 @@ firstImg = carousel.querySelectorAll("img") [0];
 arrowIcons = document.querySelectorAll(".wrapper i");
 
 let isDragStart = false, prevPageX, prevScrollLeft;
-let firstImgWidth = firstImg.clientWidth + 14;
-let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+
 
 const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", ()=> {
+        let firstImgWidth = firstImg.clientWidth + 14;
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
         setTimeout(() => showHideIcons(),);
     })
