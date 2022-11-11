742c6e623a2d980ac47ea163a5d1020864f733fd
Gibson uwhetu <vellagibbs@gmail.com>
2022-11-11 12:37:23 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -43,3 +43,4 @@ const dragStop = () => {
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
